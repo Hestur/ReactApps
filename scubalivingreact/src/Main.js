4f8b21663706a3c6feb4e95a10c0561609df9a13
hesten <spamhesten72@live.dk>
2019-03-28 08:58:10 +0100

    GotoParisReact, RunIncReact og ScubalivingReact added

---
+++ scubalivingreact/src/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HomeSection from "./HomeSection";
+import UdstyrSection from "./UdstyrSection";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main className="main">
+        <HomeSection />
+        <UdstyrSection />
+        {}
+      </main>
+    );
+  }
+}
+
+export default Main;
