240e990b63fd0fe2e6cdde55a9457d488ebddcb1
hesten <spamhesten72@live.dk>
2019-04-12 10:54:50 +0200

    authadded

--- marioplan/src/components/auth/SignIn.js
+++ marioplan/src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions';
 
 class SignIn extends Component {
     state = {
@@ -13,12 +15,13 @@ password: ''
     }
     handleSubmit = (e) =>{
         e.preventDefault();
-        console.log(this.state)
+        this.props.signIn(this.state)
     }
 
 
 
 render() {
+    const {authError} = this.props
     return(
         <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -33,10 +36,25 @@ render() {
             </div>
             <div className="input-field">
                 <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div className="red-text center">
+            { authError ? <p>{authError}</p>: null }
+            </div>
             </div>
         </form>
         </div>
     )
 }
 }
-export default SignIn
+
+const mapDispathToProps = (dispatch) => {
+    return{
+        signIn: (creds) => dispatch(signIn(creds))
+    }
+}
+const mapStateToProps = (state) => {
+    return{
+        authError: state.auth.authError
+    }
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(SignIn)
