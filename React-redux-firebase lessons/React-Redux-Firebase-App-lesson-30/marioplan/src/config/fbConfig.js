68b06b1446543010d7ac2409a373657a4e5f57b9
hesten <spamhesten72@live.dk>
2019-05-27 11:50:40 +0200

    ll

---
+++ React-redux-firebase lessons/React-Redux-Firebase-App-lesson-30/marioplan/src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+var config = {
+  apiKey: "AIzaSyCcgXAitQinuroNgeZyRnMWyrQEGdqvmmM",
+  authDomain: "hest-marioplan.firebaseapp.com",
+  databaseURL: "https://hest-marioplan.firebaseio.com",
+  projectId: "hest-marioplan",
+  storageBucket: "hest-marioplan.appspot.com",
+  messagingSenderId: "938975609451"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase 
--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { reduxFirestore ,getFirestore } from 'redux-firestore'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
-import fbConfig from './config/fbConfig'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(fbConfig, {
+      userProfile: "users",
+      useFirestoreForProfile: true,
+      attachAuthIsReady: true
+    }),
+    reduxFirestore(fbConfig) // redux bindings for firestore
+  )
+);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
-const store = createStore(rootReducer, compose(
-     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-       reduxFirestore(fbConfig),
-       reactReduxFirebase(fbConfig, {useFirestoreForProfile: true, userProfile: 'users', attachAuthIsReady: true})
-     )
-     );
-
-store.firebaseAuthIsReady.then(()=> {
-  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-  serviceWorker.unregister();
-})
-
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+});
