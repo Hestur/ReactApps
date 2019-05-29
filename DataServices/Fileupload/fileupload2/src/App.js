cf8021689d60c69a0158265e1d7ef820c3348acf
hesten <spamhesten72@live.dk>
2019-05-29 09:26:29 +0200

    jj

--- DataServices/Fileupload/fileupload2/src/App.js
+++ DataServices/Fileupload/fileupload2/src/App.js
@@ -57,30 +57,35 @@ class App extends Component {
 }
 }
 
-  checkMimeType=(event)=>{
-    //getting file object
-    let files = event.target.files 
-    //define message container
-    let err = ''
-    // list allow mime type
-   const types = ['image/png', 'image/jpeg', 'image/gif']
-    // loop access array
-    for(var x = 0; x<files.length; x++) {
-     // compare file type find doesn't matach
-         if (types.every(type => files[x].type !== type)) {
-         // create error message and assign to container   
-         err += files[x].type+' is not a supported format\n';
-       }
-     };
-  
-   if (err !== '') { // if message not same old that mean has error 
-        event.target.value = null // discard selected file
-        console.log(err)
-         return false; 
-    }
-   return true;
-  
+checkType = (t) => {
+  // list allow mime type
+  const types = ['image/png', 'image/jpeg', 'image/gif']
+  return types.every(type => t !== type);
+}
+
+// Check mimetypes
+checkMimeType = (event) => {
+  //getting file object
+  let files = event.target.files;
+  //define message container
+  let err = '';
+
+  // loop access array
+  for (var x = 0; x < files.length; x++) {
+      // compare file type find doesn't matach
+      if (this.checkType(files[x].type)) {
+          err += files[x].type + ' is not a supported format\n'; // create error message and assign to container 
+      }
+  };
+
+  if (err !== '') { // if message not same old that mean has error 
+      event.target.value = null // discard selected file
+      console.log(err);
+      alert(err);
+      return false;
   }
+  return true;
+}
 
   onChangeHandler=event=>{
     var files = event.target.files
